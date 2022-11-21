80b459b883bf5b156563625615fe9966ac096e90
David <grigorenkodavid@gmail.com>
2022-11-21 20:53:18 +0300

    Added graph script.

---
+++ graph.py
@@ -0,0 +1,48 @@
+import json
+import sys
+
+import jmespath
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    _, data_path, picture_path = sys.argv
+
+    with open(data_path) as f:
+        data = json.load(f)
+
+    x = np.array(data["x"])
+    pressure = np.array(jmespath.search("solution[*].pressure", data))
+    density = np.array(jmespath.search("solution[*].density", data))
+    velocity = np.array(jmespath.search("solution[*].velocity", data))
+    mach_number = np.array(jmespath.search("solution[*].mach_number", data))
+
+    fig, ((ax_1, ax_2), (ax_3, ax_4)) = plt.subplots(2, 2, figsize=(10, 8), dpi=200)
+
+    ax_1.plot(x, pressure)
+    ax_1.set_title("Pressure")
+    ax_1.set_ylabel("$p$")
+    ax_1.set_xlabel("$x$")
+    ax_1.grid(True)
+
+    ax_2.plot(x, density)
+    ax_2.set_title("Density")
+    ax_2.set_ylabel("$\\rho$")
+    ax_2.set_xlabel("$x$")
+    ax_2.grid(True)
+
+    ax_3.plot(x, velocity)
+    ax_3.set_title("Velocity")
+    ax_3.set_ylabel("$u$")
+    ax_3.set_xlabel("$x$")
+    ax_3.grid(True)
+
+    ax_4.plot(x, mach_number)
+    ax_4.set_title("Mach number")
+    ax_4.set_ylabel("$M$")
+    ax_4.set_xlabel("$x$")
+    ax_4.grid(True)
+
+    fig.tight_layout(pad=1.3)
+    fig.savefig(picture_path)
